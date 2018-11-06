6e27916f9b3b255ea368951fe2950846516c963a
geweitong <13898712211@163.com>
2018-11-06 16:47:54 +0800

    完成登录框

--- src/mixins/audio.mixin.js
+++ src/mixins/audio.mixin.js
@@ -2,7 +2,7 @@ export default {
     data(){
         return {
             onlineAu: this.createAudio('online-didi'),
-            offlineAu: document.createElement("audio"),
+            // offlineAu: this.createAudio("audio"),
         }
     },
     methods: {
--- src/views/login/src/components/login.vue
+++ src/views/login/src/components/login.vue
@@ -2,9 +2,10 @@
     <div class="wp-login-box">
         <div class="wp-login-box__main">
             <div class="wp-login-box__content">
-                <div class="content-title">账号密码登录</div>
+                <h2 class="content-title">账号密码登录</h2>
+                <h3 class="content-subtitle">请使用您的<span>旋风号</span>登录</h3>
                 <div class="content-input input-userid">
-                    <input type="text" placeholder="请输入您的旋风号">
+                    <input type="text" placeholder="请输入您的账号">
                 </div>
                 <div class="content-input input-pwd">
                     <input type="password" placeholder="密码">
@@ -12,8 +13,8 @@
                 <div class="content-btn login-btn">登录</div>
             </div>
             <div class="wp-login-box__bottom">
-                <a href="">忘记密码</a>
-                <a href="">注册新账号</a>
+                <a class="bottom-btn" href="#">忘记密码</a>
+                <a class="bottom-btn" href="#">注册新账号</a>
             </div>
         </div>
     </div>
@@ -44,13 +45,73 @@ export default {
         border-radius: 20px;
         text-align: center;
         box-sizing: border-box;
-        padding: 20px 0;
+        padding-top: 25px;
     }
     @include e(content) {
-
+        .content-title {
+            font-size: 24px;
+            font-weight: normal;
+            margin-bottom: 16px;
+        }
+        .content-subtitle {
+            font-weight: normal;
+            font-size: 16px;
+            margin-bottom: 16px;
+            span {
+                color: #4782c9;
+                margin: 0 5px;
+            }
+        }
+        .content-input {
+            input {
+                width: 400px;
+                height: 42px;
+                border: solid 1px #e1e1e1;
+                margin-bottom: 15px;
+                border-radius: 6px;
+                font-size: 14px;
+                &:focus {
+                    border-color: #4782c9;
+                }
+            }
+        }
+        .content-btn {
+            margin: 0 auto;
+            width: 402px;
+            height: 46px;
+            background-color: #97ca4d;
+            color: white;
+            border-radius: 6px;
+            line-height: 46px;
+            cursor: pointer;
+        }
     }
     @include e(bottom) {
-
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        padding: 10px 28px;
+        .bottom-btn {
+            font-size: 14px;
+            color: #666666;
+            &:nth-of-type(1) {
+                position: relative;
+                margin-right: 5px;
+                &:after {
+                    content: '';
+                    position: absolute;
+                    right: -6px;
+                    top: 50%;
+                    transform: translateY(-50%);
+                    height: 14px;
+                    width: 2px;
+                    background-color: #c0c0c0;
+                }
+            }
+            &:hover {
+                text-decoration: underline;
+            }
+        }
     }
 }
 </style>
